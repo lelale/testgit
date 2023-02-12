d17cee201d99ab920fc6c9000863d31a38103db1
lelale <sky.water.tree@gmail.com>
2023-02-12 13:31:22 +0800

    linked list initialization
    linked list initialization

---
+++ MyHashSet.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+
+class MyHashSet {
+    
+    private LinkedList<Integer>[] BucketArray ;
+    // private LinkedList<Integer>[] Buckets = new LinkedList<Integer>[100];
+    private int SetRange;
+
+    public MyHashSet() {
+        this.SetRange = 1000;
+        this.BucketArray = new LinkedList[SetRange];
+        for (int i = 0; i < SetRange; i++) {
+            this.BucketArray[i] = new LinkedList<Integer>();
+        }
+    }
+    
+    public void add(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+    
+    public boolean contains(int key) {
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        MyHashSet mhs = new MyHashSet();
+        System.out.println(mhs.BucketArray[1]);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
