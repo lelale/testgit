32ec894337f714a76c04f56398134c6801211d25
lelale <sky.water.tree@gmail.com>
2023-02-15 21:07:42 +0800

    correct remove

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,35 +1,79 @@
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+
+
+
 
 class MyHashSet {
     
-    private LinkedList<Integer>[] BucketArray ;
+    private static LinkedList<Integer>[] BucketArray ;
     private int SetRange;
     
     @SuppressWarnings("unchecked")
     public MyHashSet() {
-        this.SetRange = 1000;
-        this.BucketArray =  new LinkedList[SetRange];
+        this.SetRange = 5;
+        BucketArray =  new LinkedList[SetRange];
         for (int i = 0; i < SetRange; i++) {
-            this.BucketArray[i] = new LinkedList<Integer>();
+            BucketArray[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public static void PrintBucket(){
+        for(LinkedList<Integer> b : BucketArray) {
+            Iterator<Integer> it = b.iterator();
+            while (it.hasNext()) {
+                System.out.println(it.next());
+            }
+        }
+        System.out.println("----");
+
+    }
+
+    protected int _hash(int key) {
+        return (key % this.SetRange);
+    }
+
+    public void add(int key) {
+        int pos = this._hash(key);
+        if (!BucketArray[pos].contains(key)) {
+            BucketArray[pos].add(key);
         }
     }
     
-    public void add(int key) {
-        
-    }
-    
     public void remove(int key) {
+        int pos = this._hash(key);
+        System.out.println(pos);
+        System.out.println(key);
+        System.out.println(BucketArray[pos].size());
+        
+        if(!BucketArray[pos].isEmpty() && BucketArray[pos].contains(key)){
+            BucketArray[pos].remove(key);
+            // BucketArray[pos].remove(Integer.valueOf(key));
+        }
         
     }
     
     public boolean contains(int key) {
-        return true;
+        int pos = this._hash(key);
+        return (BucketArray[pos].contains(key));
     }
 
 
     public static void main(String[] args){
         MyHashSet mhs = new MyHashSet();
-        System.out.println(mhs.BucketArray[1]);
+        mhs.add(20);
+        PrintBucket();
+        mhs.remove(20);
+        PrintBucket();
+        mhs.add(50);
+        mhs.add(50);
+        mhs.add(50);
+        mhs.remove(50);
+        mhs.contains(50);      
     }
 }
 
