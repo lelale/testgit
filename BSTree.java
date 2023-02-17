aa2a6c57b4e75b31a2ed8cb1ed7e4d9e338ac038
lelale <sky.water.tree@gmail.com>
2023-02-17 21:32:07 +0800

    delete node implement version1
    1. null tree 2.one node tree(special leaf) 3. delete leaf (need father node) 4.delete node with left OR right child  5.delete node with left AND right child

--- BSTree.java
+++ BSTree.java
@@ -3,6 +3,7 @@
 public class BSTree {
     TreeNode root = null;
 
+    // return the target node
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null || val == root.val) {
             return root;
@@ -65,7 +66,103 @@ public class BSTree {
     //     return root;
     // }
 
+    //return the largest number of the left tree
+    public int predecessor(TreeNode root, int val){
+        TreeNode pre = root.left;
+        int preVal = pre.val;
+        while (pre != null){
+            preVal = pre.val;
+            pre = pre.right;
+        }
+        return preVal;
+    }
+
+    //return the smallest number of the right tree
+    public int successor(TreeNode root, int val){
+        TreeNode suc = root.right;
+        int sucVal = suc.val;
+        while (suc != null){
+            sucVal = suc.val;
+            suc = suc.left;
+        }
+        return sucVal;
+    }
     
+    // It deletes the node of a given value of a BSTree
+    // It takes with the root of the tree and the vlaue as input, and it returns the root of the updated tree
+    public TreeNode deleteNode(TreeNode root, int val){
+        TreeNode delete = root;
+        TreeNode father = null;
+        if (root == null){
+            return null;
+        }
+
+        // find the node to delete and its father node
+        while (root != null ){
+            if (val == root.val){
+                delete = root;
+                break;
+            }
+            else if (val > root.val){
+                if(root.right.val == val){
+                    father = root;
+                    delete = root.right;
+                    break;
+                }
+                else {
+                    root = root.right;
+                }
+            }
+            else if (val < root.val){
+                if(root.left.val == val){
+                    father = root;
+                    delete = root.left;
+                    break;
+                }
+                else {
+                    root = root.left;
+                }
+            }
+        }
+
+        // delte leaf node
+        if(delete.left == null && delete.right == null){
+            if (father == null){
+                return null;
+            }
+            else {
+                if(father.val > val){
+                    father.left = null;
+                    return root;
+                }
+                else if(father.val < val){
+                    father.right = null;
+                    return root;
+                }
+            }
+        }
+        else if(delete.left == null){
+            TreeNode right = delete.right;
+            delete.val = right.val;
+            delete.left = right.left; 
+            delete.right = right.right;
+        }
+        else if (delete.right == null){
+            TreeNode left = delete.left;
+            delete.val = left.val;
+            delete.left = left.left; 
+            delete.right = left.right;
+        }
+        else{
+            int suc = successor(root, val);
+            deleteNode(root, suc);
+            delete.val = suc;
+
+            
+        }
+        return root;
+
+    }
 
 
     public void printTree(TreeNode root) {
@@ -84,14 +181,14 @@ public class BSTree {
         BSTree tree = new BSTree();
 
         tree.root = tree.insertIntoBST(tree.root, 5);
-   
-
         tree.insertIntoBST(tree.root, 2);
         tree.insertIntoBST(tree.root, 7);
         tree.insertIntoBST(tree.root, 1);
         tree.insertIntoBST(tree.root, 3);
         tree.insertIntoBST(tree.root, 6);
         tree.insertIntoBST(tree.root, 8);
+
+        tree.deleteNode(tree.root,8);
 
         tree.printTree(tree.root);
     }
