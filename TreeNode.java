9bb21fa35b430db3952ae86f3088e96f8b24cf6e
lelale <sky.water.tree@gmail.com>
2023-02-18 14:17:26 +0800

    test unit
    test unit

--- TreeNode.java
+++ TreeNode.java
@@ -97,24 +97,55 @@ class MyHashSet {
       MyHashSet mhs = new MyHashSet();
       // LinkdedList<String> InputList = new LinkedList[String]{"MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"
     //  String[] commands = {"MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"};
-     String[] commands = {"MyHashSet","contains","remove","add","add","contains","remove","contains","contains","add","add","add","add","remove","add","add","add",
+     String[] commands = {"contains","remove","add","add","contains","remove","contains","contains","add","add","add","add","remove","add","add","add",
      "add","add","add","add","add","add","add","contains","add","contains","add","add","contains","add","add","remove","add","add","add","add","add","contains","add",
      "add","add","remove","contains","add","contains","add","add","add","add","add","contains","remove","remove","add","remove","contains","add","remove","add","add",
      "add","add","contains","contains","add","remove","remove","remove","remove","add","add","contains","add","add","remove","add","add","add","add","add","add","add",
      "add","remove","add","remove","remove","add","remove","add","remove","add","add","add","remove","remove","remove","add","contains","add"};
-     int[][] nums = {{},{72},{91},{48},{41},{96},{87},{48},{49},{84},{82},{24},{7},{56},{87},{81},{55},{19},{40},{68},{23},{80},{53},{76},{93},{95},{95},{67},{31},
+     int[][] nums = {{72},{91},{48},{41},{96},{87},{48},{49},{84},{82},{24},{7},{56},{87},{81},{55},{19},{40},{68},{23},{80},{53},{76},{93},{95},{95},{67},{31},
      {80},{62},{73},{97},{33},{28},{62},{81},{57},{40},{11},{89},{28},{97},{86},{20},{5},{77},{52},{57},{88},{20},{48},{42},{86},{49},{62},{53},{43},{98},{32},{15},
      {42},{50},{19},{32},{67},{84},{60},{8},{85},{43},{59},{65},{40},{81},{55},{56},{54},{59},{78},{53},{0},{24},{7},{53},{33},{69},{86},{7},{1},{16},{58},{61},{34},
      {53},{84},{21},{58},{25},{45},{3}};
+     Object[] officalResult = {false,null,null,null,false,null,false,false,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,false,null,true,null,null,true,null,null,null,null,null,null,null,null,false,null,null,null,null,false,null,false,null,null,null,null,null,false,null,null,null,null,false,null,null,null,null,null,null,false,true,null,null,null,null,null,null,null,false,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,false,null};
      List<Integer> numsArray = new ArrayList<>();
      for(int[] row : nums){
       for(int num: row){
         numsArray.add(num);
       }
      }
-     System.out.println(numsArray.size() == commands.length);
+      //  System.out.println(numsArray.size() == commands.length);
+    //  System.out.println(numsArray.size());
+    //  System.out.println(commands.length);
+    Object[] result = new Object[100];
+
+     for(int i = 0; i< numsArray.size(); i++){
+        String op = commands[i];
+        int val = numsArray.get(i);
+        switch(op){
+          case "add":
+            result[i] = null;
+            mhs.add(val);
+          case "remove":
+          result[i] = null;
+           mhs.remove(val);
+          case "contains":
+          result[i] = mhs.contains(val);
+        }
+     }
+
+     System.out.println(numsArray.size() == result.length);
      System.out.println(numsArray.size());
-     System.out.println(commands.length);
+     System.out.println(result.length);
+     System.out.println(officalResult.length);
+
+     for(int i = 0; i< numsArray.size(); i++){
+      if(result[i]!= null && officalResult[i]!= null && (result[i].toString() != officalResult[i].toString())){
+        System.out.println(i);
+        System.out.println(commands[i]);
+        System.out.println(numsArray.get(i));
+        System.out.println("---notmatch----");
+      }
+     }
 
     }
            
